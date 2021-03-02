b18277009255d5555e4280c9c9a17c541207db24
AguerWinceville <aguerwinceville@gmail.com>
2021-03-02 12:34:36 +0300

    12:34 pm

--- app/src/main/java/manu/apps/androidcodingstarterpack/fragments/ChipsGroupFragment.java
+++ app/src/main/java/manu/apps/androidcodingstarterpack/fragments/ChipsGroupFragment.java
@@ -1,24 +1,34 @@
 package manu.apps.androidcodingstarterpack.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
 import manu.apps.androidcodingstarterpack.R;
 
-public class ChipsGroupFragment extends Fragment {
+public class ChipsGroupFragment extends Fragment implements View.OnClickListener {
 
     ChipGroup cgGender;
+    Chip cMale, cFemale;
+
+    MaterialButton btnCheckSelectedChip;
+
+    String gender;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -36,17 +46,81 @@ public class ChipsGroupFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         cgGender = view.findViewById(R.id.cg_gender);
+        cMale = view.findViewById(R.id.c_male);
+        cFemale = view.findViewById(R.id.c_female);
+        btnCheckSelectedChip = view.findViewById(R.id.btn_check_selected_chip);
 
-        cgGender.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(ChipGroup group, int checkedId) {
+        cMale.setOnClickListener(v -> {
 
-                Chip chip = cgGender.findViewById(checkedId);
+            cMale.setChipIconVisible(true);
 
-                if (chip != null) {
-                    Toast.makeText(requireActivity(), "Chip is " + chip.getText(), Toast.LENGTH_LONG).show();
+            cMale.setChipBackgroundColor(AppCompatResources.
+                    getColorStateList(requireActivity(), R.color.colorPrimary));
+
+            cMale.setChipIcon(ContextCompat.getDrawable(requireActivity(),
+                    R.drawable.ic_check));
+
+            cFemale.setChipBackgroundColor(AppCompatResources.
+                    getColorStateList(requireActivity(), R.color.colorAccent));
+
+            cFemale.setChipIconVisible(false);
+
+        });
+
+        cFemale.setOnClickListener(v -> {
+
+            cFemale.setChipIconVisible(true);
+
+            cFemale.setChipBackgroundColor(AppCompatResources.
+                    getColorStateList(requireActivity(), R.color.colorPrimary));
+
+            cFemale.setChipIcon(ContextCompat.getDrawable(requireActivity(),
+                    R.drawable.ic_check));
+
+            cMale.setChipBackgroundColor(AppCompatResources.
+                    getColorStateList(requireActivity(), R.color.colorAccent));
+
+            cMale.setChipIconVisible(false);
+
+        });
+
+        cgGender.setOnCheckedChangeListener((group, checkedId) -> {
+
+            Chip chip = cgGender.findViewById(checkedId);
+
+            if (chip != null) {
+
+                Toast.makeText(requireActivity(), "Chip is " + chip.getText(), Toast.LENGTH_LONG).show();
+
+                gender = chip.getText().toString();
+
+            }
+
+        });
+
+        btnCheckSelectedChip.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int viewId = v.getId();
+
+        if (viewId == R.id.btn_check_selected_chip){
+
+            if (cgGender.getCheckedChipId() == -1){
+                Toast.makeText(requireActivity(), "No Chip has been selected", Toast.LENGTH_SHORT).show();
+            }else {
+
+                if (!TextUtils.isEmpty(gender)){
+                    new AlertDialog.Builder(requireActivity())
+                            .setMessage("Selected Gender is:" + gender)
+                            .show();
+                }else {
+                    Toast.makeText(requireActivity(), "No Chip has been selected", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
+        }
     }
 }
